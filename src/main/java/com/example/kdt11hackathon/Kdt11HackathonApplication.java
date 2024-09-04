b4207c52aa2a027b4232444d2957c196e3686283
kongji <kongji4092@gmail.com>
2024-09-04 18:21:22 +0900

    initial commit

---
+++ src/main/java/com/example/kdt11hackathon/Kdt11HackathonApplication.java
@@ -0,0 +1,13 @@
+package com.example.kdt11hackathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kdt11HackathonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Kdt11HackathonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kdt11hackathon/Kdt11HackathonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kdt11hackathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kdt11HackathonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
